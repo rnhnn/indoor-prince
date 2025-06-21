c8a94177740a76ae6980a9d6208bb37a5c243c34
Rolando <rolandohernandezleal@gmail.com>
2025-06-21 12:40:27 -0300

    update rss script again

--- scripts/generateRSS.js
+++ scripts/generateRSS.js
@@ -9,9 +9,13 @@ const posts = getAllPosts();
 const itemsXml = posts
   .map((post) => {
     const fullContent = `
-      <img src="${siteURL}/images/blog/${post.image}" alt="${post.alt}" />
-      ${post.content.map(block => block.html).join('')}
+      <img src="${siteURL}/images/blog/${post.image}" alt="${post.alt}" style="max-width:100%; height:auto;" />
+      ${post.content.map((block) => block.html).join('')}
     `;
+
+    const plainDescription = post.content[0]?.html
+      ?.replace(/<[^>]*>/g, '') // Strip HTML tags
+      .slice(0, 200); // Optional: trim to 200 chars
 
     return `
       <item>
@@ -19,6 +23,7 @@ const itemsXml = posts
         <link>${siteURL}/${post.slug}</link>
         <guid>${siteURL}/${post.slug}</guid>
         <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+        <description><![CDATA[${plainDescription}]]></description>
         <content:encoded><![CDATA[${fullContent}]]></content:encoded>
       </item>
     `;
@@ -31,7 +36,7 @@ const rssXml = `
     <channel>
       <title>Indoor Prince</title>
       <link>${siteURL}</link>
-      <description>A blog about video games and more</description>
+      <description>A site about video games</description>
       <language>en-us</language>
       <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
       <image>
